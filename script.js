9a2502520a7e4c3d828dcfec621e6fd87d2a8dab
Alice Almeida <alice.pereira13@gmail.com>
2022-10-17 16:03:08 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let pratoEscolhido;
+let pratoEscolhido, sobremesa, total = 0;
 
 function escolherPrato(botao) {
     const botaoSelecionado = document.querySelector(".pratos .selecionado");
     if (botaoSelecionado !== null) {
-        botaoSelecionado.classList.remove("selecionado");
+        botaoSelecionado.classList.remove("selecionado")
+            ;
     }
-    botao.classList.toggle("selecionado");
-
-    pratoEscolhido = botao.innerHTML;
+    botao.classList.add("selecionado");
+    console.log(botao)
+    pratoEscolhido = botao.querySelector(".valor-pago")
+    if (pratoEscolhido !== null) {
+        pratoEscolhido = pratoEscolhido.innerHTML;
+        total += pratoEscolhido;
+    }
+    console.log(total)
 
     fecharPedido();
 }
@@ -18,24 +24,39 @@ function escolherBebida(botao2) {
     const botaoSelecionado2 = document.querySelector(".bebidas .selecionado");
     if (botaoSelecionado2 !== null) {
         botaoSelecionado2.classList.remove("selecionado");
+        total += bebidaEscolhida;
     }
-    botao2.classList.toggle("selecionado");
-
-    bebidaEscolhida = botao2.innerHTML;
+    botao2.classList.add("selecionado");
+    console.log(botao2)
+    bebidaEscolhida = botao2.querySelector(".valor-pago")
+    if (bebidaEscolhida !== null) {
+        bebidaEscolhida = bebidaEscolhida.innerHTML;
+        total += bebidaEscolhida;
+    }
+    console.log(total)
 
     fecharPedido();
 }
 
-let doceEscolhido;
+let sobremesaEscolhido;
 
 function escolherSobremesa(botao3) {
     const botaoSelecionado3 = document.querySelector(".sobremesa .selecionado");
     if (botaoSelecionado3 !== null) {
         botaoSelecionado3.classList.remove("selecionado");
+        total += sobremesaEscolhido;
     }
-    botao3.classList.toggle("selecionado");
-
+    botao3.classList.add("selecionado");
+    sobremesaEscolhido = botao3.querySelector(".valor-pago")
+    console.log(sobremesaEscolhido)
     sobremesaEscolhido = botao3.innerHTML;
+    sobremesaEscolhido = botao3.querySelector(".valor-pago")
+    if (sobremesaEscolhido !== null) {
+        sobremesaEscolhido = sobremesaEscolhido.innerHTML;
+        total += sobremesaEscolhido;
+    }
+    console.log(total)
+
 
     fecharPedido();
 }
@@ -51,3 +72,4 @@ function fecharPedido() {
         botaoFechar.innerHTML = "Fechar Pedido";
     }
 }
+
