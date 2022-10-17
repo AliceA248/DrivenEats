b913724bea884b3b2a3cb6ca11d351f379edc58e
Alice Almeida <alice.pereira13@gmail.com>
2022-10-17 15:25:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+let pratoEscolhido;
+
+function escolherPrato(botao) {
+    const botaoSelecionado = document.querySelector(".pratos .selecionado");
+    if (botaoSelecionado !== null) {
+        botaoSelecionado.classList.remove("selecionado");
+    }
+    botao.classList.toggle("selecionado");
+
+    pratoEscolhido = botao.innerHTML;
+
+    fecharPedido();
+}
+
+let bebidaEscolhida;
+
+function escolherBebida(botao2) {
+    const botaoSelecionado2 = document.querySelector(".bebidas .selecionado");
+    if (botaoSelecionado2 !== null) {
+        botaoSelecionado2.classList.remove("selecionado");
+    }
+    botao2.classList.toggle("selecionado");
+
+    bebidaEscolhida = botao2.innerHTML;
+
+    fecharPedido();
+}
+
+let doceEscolhido;
+
+function escolherSobremesa(botao3) {
+    const botaoSelecionado3 = document.querySelector(".sobremesa .selecionado");
+    if (botaoSelecionado3 !== null) {
+        botaoSelecionado3.classList.remove("selecionado");
+    }
+    botao3.classList.toggle("selecionado");
+
+    sobremesaEscolhido = botao3.innerHTML;
+
+    fecharPedido();
+}
+
+function fecharPedido() {
+    if (
+        pratoEscolhido !== undefined &&
+        bebidaEscolhida !== undefined &&
+        sobremesaEscolhido !== undefined
+    ) {
+        const botaoFechar = document.querySelector(".botao");
+        botaoFechar.classList.add("escondido");
+        botaoFechar.innerHTML = "Fechar Pedido";
+    }
+}
